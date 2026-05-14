82ae13cb9847584d29a4c2fb38fbb7ba5b64c335
Dawit Chernet <davidintsum@gmail.com>
2026-05-13 23:54:29 -0400

    including the resume information

---
+++ script.js
@@ -0,0 +1,38 @@
+const typingText = document.getElementById('typing-text');
+const roles = [
+    "Solution Architect",
+    "Senior Software Developer",
+    "PMP Certified Expert",
+    "BSS/OSS Catalog Architect"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentRole = roles[roleIndex];
+    
+    if (isDeleting) {
+        typingText.textContent = currentRole.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        typingText.textContent = currentRole.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    let typeSpeed = isDeleting ? 50 : 150;
+
+    if (!isDeleting && charIndex === currentRole.length) {
+        typeSpeed = 2000;
+        isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        roleIndex = (roleIndex + 1) % roles.length;
+        typeSpeed = 500;
+    }
+
+    setTimeout(type, typeSpeed);
+}
+
+document.addEventListener('DOMContentLoaded', type);
